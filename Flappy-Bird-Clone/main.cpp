1c50baac12a9078eb599483428859b5b4c87264b
Túlio Henrique <tuliohdvp@gmail.com>
2019-02-03 16:29:01 -0200

    Initial Commit

---
+++ Flappy-Bird-Clone/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Flappy-Bird-Clone
+//
+//  Created by Túlio Henrique on 03/02/19.
+//  Copyright © 2019 Túlio Henrique. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
